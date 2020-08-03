c1da61095451b7377200ee31b4b0631653f285e2
Carmen Alvarez <calvarez@libon.com>
2020-08-03 15:27:59 +0200

    Add a button which makes the app crash in the linphone sdk

--- app/src/main/java/com/example/crashobfuscationtest/MainActivity.kt
+++ app/src/main/java/com/example/crashobfuscationtest/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.crashobfuscationtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import org.linphone.core.Factory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnForceCrash = findViewById<Button>(R.id.btn_force_crash)
+        btnForceCrash.setOnClickListener { forceCrash() }
+    }
+
+    private fun forceCrash() {
+        Factory.instance().setDebugMode(true, "Linphone")
+        val core = Factory.instance().createCore(null, null, this.getApplicationContext())
+        core.start()
+        core.adaptiveRateAlgorithm = null
     }
 }
